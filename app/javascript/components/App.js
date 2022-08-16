31bd338cc3b8f988173ebe542b11a3af0f575b8a
aboongm <aboongm@yahoo.com>
2022-08-16 09:33:35 +0530

    Create the redux folder and create the configureStore.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import { Provider } from 'react-redux';
 import {  BrowserRouter, Routes, Route } from 'react-router-dom';
 import Greeting from './Greeting';
 import Header from './Header';
 import Home from './Home';
+import store from '../redux/ConfigureStore';
 
 const App = () => {
   return (
-    
+    <Provider store={store}>
         <BrowserRouter>
             <Header />
             <Routes>
@@ -25,7 +26,7 @@ const App = () => {
                     />
             </Routes>
         </BrowserRouter>
-    
+    </Provider>
   )
 }
 
