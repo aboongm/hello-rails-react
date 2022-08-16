5c78be26d060b06dc0ce99caa1561db4c73e6108
aboongm <aboongm@yahoo.com>
2022-08-16 10:46:34 +0530

    Update Home component

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Home = () => {
+const Home =()=>{
   return (
-    <div>Home</div>
+    <div class="bg-slate-200 mt-20 flex flex-col items-center my-4 mx-12 border p-5">
+      <p className="text-2xl">Home</p>
+    </div>
   )
 }
 
