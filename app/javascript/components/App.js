5aefa1f58a01dfa9ba4d019f598bed7cf4488c82
aboongm <aboongm@yahoo.com>
2022-08-16 09:30:04 +0530

    Install react-router-dom, redux, react-redux, create the routes in App.js and create header, home and greeting components

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
+import {  BrowserRouter, Routes, Route } from 'react-router-dom';
+import Greeting from './Greeting';
+import Header from './Header';
+import Home from './Home';
+
 const App = () => {
   return (
-    <div>App</div>
+    
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route
+                    path="/"
+                    element={(
+                        <Home />
+                    )}
+                    />
+                <Route
+                    path="/greeting"
+                    element={(
+                        <Greeting />
+                    )}
+                    />
+            </Routes>
+        </BrowserRouter>
+    
   )
 }
 
 export default App
-
